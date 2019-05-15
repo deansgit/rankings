a836d3eb8a4b9359a32a90b6422587a87f0a7741
xuo <duecking.sascha@gmail.com>
2019-05-15 06:18:28 +0200

    Update mockData, add default-container flex wrap

--- src/components/Tiermaker/Tiermaker.jsx
+++ src/components/Tiermaker/Tiermaker.jsx
@@ -3,12 +3,52 @@ import React, { useReducer } from 'react'
 import DefaultArea from './DefaultArea'
 import { DragDropContext } from 'react-beautiful-dnd'
 import RowContainer from './RowContainer'
-import mockData from '../../mockData.json'
 import reducer from './reducer'
 import { tail } from '../../util'
 
 export const TiermakerContext = React.createContext(null)
-const initialState = mockData
+
+const IMAGE_LIST = [
+  'https://i.imgur.com/oZrrEk9.png',
+  'https://i.imgur.com/holSvy7.png',
+  'https://i.imgur.com/33vdZnY.png',
+  'https://i.imgur.com/lIc3gAm.png',
+  'https://i.imgur.com/sXGKqr1.png',
+  'https://i.imgur.com/TsJzbfd.png',
+  'https://i.imgur.com/0U3vV2b.png',
+  'https://i.imgur.com/0ahe4dB.png',
+  'https://i.imgur.com/y6WYLNq.png',
+  'https://i.imgur.com/NUPVPIh.png',
+  'https://i.imgur.com/Lh3czPn.png',
+  'https://i.imgur.com/Y05LJUk.png',
+  'https://i.imgur.com/3OvYK23.png',
+  'https://i.imgur.com/pxqC7UL.png',
+  'https://i.imgur.com/4PnnBeo.png',
+  'https://i.imgur.com/LQz8Pag.png',
+  'https://i.imgur.com/OSxJTMm.png',
+  'https://i.imgur.com/3LuP2CN.png',
+  'https://i.imgur.com/FijLWMi.png',
+  'https://i.imgur.com/0EmXish.png',
+  'https://i.imgur.com/bHFXkc2.png'
+]
+
+function createInitialState(imageList) {
+  const data = []
+  const defaultArea = imageList.map((url, i) => {
+    return { name: `Item ${i + 1}`, image: url }
+  })
+  data.push(defaultArea)
+  const rows = [
+    { name: 'Love', color: 'green', items: [] },
+    { name: 'Like', color: 'lightgreen', items: [] },
+    { name: 'Dislike', color: 'orange', items: [] },
+    { name: 'Hate', color: 'red', items: [] }
+  ]
+  rows.map(row => data.push(row))
+  return data
+}
+
+export const initialState = createInitialState(IMAGE_LIST)
 
 function Tiermaker() {
   const [data, dispatch] = useReducer(reducer, initialState)
@@ -34,6 +74,7 @@ function Tiermaker() {
 
   return (
     <TiermakerContext.Provider value={{ dispatch, data }}>
+      <button onClick={() => dispatch({ type: 'RESET' })}>Reset</button>
       <DragDropContext onDragEnd={onDragEnd}>
         <div className="container">
           {tail(data).map(({ name, color, items }, i) => (
