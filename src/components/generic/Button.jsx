460f8a568d3ae8be9ed14d05eb176e1670d87c55
xuo <duecking.sascha@gmail.com>
2019-05-17 01:54:22 +0200

    Fix nav link element semnatic (button to anchor)

--- src/components/generic/Button.jsx
+++ src/components/generic/Button.jsx
@@ -1,26 +1,6 @@
-import { getBasepath, navigate } from 'hookrouter'
-
 import React from 'react'
+import { setLinkProps } from 'hookrouter'
 
-const setLinkProps = props => {
-  const onClick = e => {
-    if (!e.shiftKey && !e.ctrlKey && !e.altKey) {
-      e.preventDefault() // prevent the link from actually navigating
-      navigate(e.currentTarget.getAttribute('href'))
-    }
-
-    if (props.onClick) {
-      props.onClick(e)
-    }
-  }
-  const href =
-    props.href.substr(0, 1) === '/' ? getBasepath() + props.href : props.href
-
-  return { ...props, href, onClick }
-}
-
-const Button = props => (
-  <button {...setLinkProps(props)}>{props.children}</button>
-)
+const Button = props => <a {...setLinkProps(props)}>{props.children}</a>
 
 export default Button
